7a16cbcb3ca462346c12a91d57030d4631b7bcce
pritam1373 <ovnandy99@gmail.com>
2025-07-29 08:13:32 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -33,3 +33,98 @@ m.addEventListener("click",()=>{
     l.style.color="blue"
 })
 
+a1=document.getElementById("myp")
+a2=document.getElementById("fp")
+a3=document.getElementById("order")
+a4=document.getElementById("like")
+a5=document.getElementById("reward")
+a6=document.getElementById("gift")
+a1.addEventListener("click",()=>{
+    a1.style.backgroundColor="blue"
+    a1.style.color="white"
+    a2.style.backgroundColor="white"
+    a2.style.color="blue"
+    a3.style.backgroundColor="white"
+    a3.style.color="blue"
+    a4.style.backgroundColor="white"
+    a4.style.color="blue"
+    a5.style.backgroundColor="white"
+    a5.style.color="blue"
+    a6.style.backgroundColor="white"
+    a6.style.color="blue"
+})  
+
+a2.addEventListener("click",()=>{
+    a2.style.backgroundColor="blue"
+    a2.style.color="white"
+    a1.style.backgroundColor="white"
+    a1.style.color="blue"
+    a3.style.backgroundColor="white"
+    a3.style.color="blue"
+    a4.style.backgroundColor="white"
+    a4.style.color="blue"
+    a5.style.backgroundColor="white"
+    a5.style.color="blue"
+    a6.style.backgroundColor="white"
+    a6.style.color="blue"
+})  
+
+a3.addEventListener("click",()=>{
+    a3.style.backgroundColor="blue"
+    a3.style.color="white"
+    a2.style.backgroundColor="white"
+    a2.style.color="blue"
+    a1.style.backgroundColor="white"
+    a1.style.color="blue"
+    a4.style.backgroundColor="white"
+    a4.style.color="blue"
+    a5.style.backgroundColor="white"
+    a5.style.color="blue"
+    a6.style.backgroundColor="white"
+    a6.style.color="blue"
+})  
+
+a4.addEventListener("click",()=>{
+    a4.style.backgroundColor="blue"
+    a4.style.color="white"
+    a2.style.backgroundColor="white"
+    a2.style.color="blue"
+    a3.style.backgroundColor="white"
+    a3.style.color="blue"
+    a1.style.backgroundColor="white"
+    a1.style.color="blue"
+    a5.style.backgroundColor="white"
+    a5.style.color="blue"
+    a6.style.backgroundColor="white"
+    a6.style.color="blue"
+})  
+
+a5.addEventListener("click",()=>{
+    a5.style.backgroundColor="blue"
+    a5.style.color="white"
+    a2.style.backgroundColor="white"
+    a2.style.color="blue"
+    a3.style.backgroundColor="white"
+    a3.style.color="blue"
+    a4.style.backgroundColor="white"
+    a4.style.color="blue"
+    a1.style.backgroundColor="white"
+    a1.style.color="blue"
+    a6.style.backgroundColor="white"
+    a6.style.color="blue"
+})  
+
+a6.addEventListener("click",()=>{
+    a6.style.backgroundColor="blue"
+    a6.style.color="white"
+    a2.style.backgroundColor="white"
+    a2.style.color="blue"
+    a3.style.backgroundColor="white"
+    a3.style.color="blue"
+    a4.style.backgroundColor="white"
+    a4.style.color="blue"
+    a5.style.backgroundColor="white"
+    a5.style.color="blue"
+    a1.style.backgroundColor="white"
+    a1.style.color="blue"
+})  
